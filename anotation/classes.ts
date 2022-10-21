b339f9ef54613ea0d3d60a943b531dfc4d85f45d
thiakovidis <thiakovidis@agroapps.gr>
2022-10-21 11:02:41 +0300

    Add classes .ts file

--- anotation/classes.ts
+++ anotation/classes.ts
@@ -1,12 +1,33 @@
 class Employee {
-  startShift(): void {
-    console.log("Employee start to shift");
-  }
-  endShift(): void {
+  constructor(public age: number) {}
+  //   public startShift(): void {
+  //     console.log("Employee start to shift");
+  //   }
+  protected endShift(): void {
     console.log("Shift has been ended");
   }
 }
 
-const employee = new Employee();
-employee.startShift();
-employee.endShift();
+class Mechanic extends Employee {
+  constructor(public name: string, age: number) {
+    super(age);
+  }
+  private startShift(): void {
+    console.log("Mechanic start to shift");
+  }
+
+  goToWork(): void {
+    this.startShift();
+    this.endShift();
+  }
+}
+
+const employee = new Employee(29);
+console.log(employee.age);
+// employee.startShift();
+// employee.endShift();
+
+const mechanic = new Mechanic(29);
+// mechanic.startShift();
+// mechanic.endShift();
+mechanic.goToWork();
